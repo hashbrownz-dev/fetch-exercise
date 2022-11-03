79d4ac6e7a9df4c384013efdecfc7c1d2d09cb83
Roosevelt Blow <rooseveltblow@gmail.com>
2022-11-03 15:23:58 -0600

    gets data from jsonplaceholder.typicode.com

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // Let's get some user data from an api using fetch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
